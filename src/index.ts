7c433dc750a4d0021955b8ec2ab3f77c19ae0833
Philip Thongkhamchanh <philipthongkhamchanh@gmail.com>
2023-12-13 11:07:53 +1300

    Refactor

--- src/index.ts
+++ src/index.ts
@@ -7,44 +7,21 @@ import { ParcelLabel } from "./parcel-label.js";
 import { ParcelTrack } from "./parcel-track.js";
 
 export class NZPost {
-    #parcelAddressChecker: AddressChecker;
+    parcelAddressChecker: AddressChecker;
 
-    #parcelAddress: ParcelAddress;
+    parcelAddress: ParcelAddress;
 
-    #parcelLabel: ParcelLabel;
+    parcelLabel: ParcelLabel;
 
-    #parcelTrack: ParcelTrack;
-
-    #clientId: string;
-
-    #clientSecret: string;
+    parcelTrack: ParcelTrack;
 
     constructor(clientId: string, clientSecret: string) {
-        this.#clientId = clientId;
-        this.#clientSecret = clientSecret;
-
-        const authentication = new Authentication(this.#clientId, this.#clientSecret);
+        const authentication = new Authentication(clientId, clientSecret);
         const apiService = new APIService(authentication);
 
-        this.#parcelAddressChecker = new AddressChecker(apiService);
-        this.#parcelAddress = new ParcelAddress(apiService);
-        this.#parcelLabel = new ParcelLabel(apiService);
-        this.#parcelTrack = new ParcelTrack(apiService);
-    }
-
-    get addressChecker() {
-        return this.#parcelAddressChecker;
-    }
-
-    get address() {
-        return this.#parcelAddress;
-    }
-
-    get label() {
-        return this.#parcelLabel;
-    }
-
-    get track() {
-        return this.#parcelTrack;
+        this.parcelAddressChecker = new AddressChecker(apiService);
+        this.parcelAddress = new ParcelAddress(apiService);
+        this.parcelLabel = new ParcelLabel(apiService);
+        this.parcelTrack = new ParcelTrack(apiService);
     }
 }
